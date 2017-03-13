edb2f6d7b391d8273206d4b24c92dfe35bf54ebc
eric <eric_solis07@hotmail.it>
2017-03-13 04:21:23 +0100

    aggiunto "postato da" e a che ora

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311193924) do
+ActiveRecord::Schema.define(version: 20170313015450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170311193924) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
+    t.integer  "user_id"
     t.text     "body"
     t.integer  "article_id"
     t.datetime "created_at", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170311193924) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                             null: false
@@ -60,4 +61,5 @@ ActiveRecord::Schema.define(version: 20170311193924) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
